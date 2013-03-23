829560258fb67cb2fe6214740da4c26abc0bd1ed
Eduard Tudenhoefner <etudenhoefner@googlemail.com>
2013-03-23 13:02:45 +0100

    fixed minor bug in the randomized selection algorithm

--- src/com/nastra/algorithms/randomizedselection.py
+++ src/com/nastra/algorithms/randomizedselection.py
@@ -11,43 +11,39 @@ that is O(n).
 '''
 import random
 
-def randomizedSelection(array, low, high, element):
-    if low == high:  # base case
-        return array[low]
+def randomizedSelection(array, k):
+    if len(array) == 1:
+        return array[0]
+    pivotIndex = randomizedPartition(array)
+    if (pivotIndex + 1) == k:
+        return array[pivotIndex]
+    elif k < (pivotIndex + 1):
+        return randomizedSelection(array[0:pivotIndex], k)
     else:
-        pivot = randomizedPartition(array, low, high)
-        if pivot == element:
-            return array[pivot]
-        elif element < pivot:
-            return randomizedSelection(array, low, pivot - 1, element)
-        else:
-            return randomizedSelection(array, pivot + 1, high, element - pivot)
-
-def randomizedPartition(array, low, high):
-    index = random.randint(low, high)
-    array[index], array[low], array[low], array[index]  # swap the two elements
-    return partition(array, low, high)
+        return randomizedSelection(array[pivotIndex + 1:len(array)] , k - (pivotIndex + 1))
     
-def partition(array, low, high):
-    x = array[low]
-    i = low + 1
-    for j in range(low + 1, high + 1):
-        if array[j] <= x:
+def randomizedPartition(array):
+    high = len(array) - 1
+    index = random.randint(0, high)
+    array[index], array[0] = array[0], array[index]  # swap the two elements
+    return partition(array)
+
+def partition(array):
+    pivot = array[0]
+    i = 1
+    for j in range(1, len(array)):
+        if array[j] <= pivot:
             array[i], array[j] = array[j], array[i]
             i += 1
-    array[i - 1], array[low], array[low], array[i - 1]
+    array[i - 1], array[0] = array[0], array[i - 1]
     return i - 1  # the index where the pivot element resides
 
+
 if __name__ == '__main__':
-    # array = [random.randint(0, 25) for i in range(0, 15)]
-    array = [9, 4, 7, 123, 55, 56, 43, 90, 34, 23]
+    array = [random.randint(0, 100) for i in range(0, 10)]
     print("Input array is: ")
     print(array)
     print("-" * 70)
-    element = 8
-    print("Searching for the " + str(element) + "th element in the input array")
-    print("Sorted output is...")
-    print(sorted(array))
-    selection = randomizedSelection(array, 0, len(array) - 1, element - 1)  # because array is zero-based
-    print("The " + str(element) + "th element is: " + str(selection))
-    pass
+    for i in range(1, len(array) + 1):
+        selection = randomizedSelection(array, i)    
+        print("The " + str(i) + "th element is: " + str(selection))
